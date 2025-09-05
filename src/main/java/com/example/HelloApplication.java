e1dfcd89aa63a418ce9bcc67889a6b35c5994ed9
SrishtiPundir12 <pundirsrishti6@gmail.com>
2025-09-05 23:50:52 +0530

    Initial Spring Boot app

---
+++ src/main/java/com/example/HelloApplication.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+
+    @RestController
+    static class Ctrl {
+        @GetMapping("/")
+        public String hi() {
+            return "Hello from Spring Boot microservice";
+        }
+    }
+}
+
